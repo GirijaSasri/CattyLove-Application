70780aae246fcbec8dc30cbe89e11952a4ef0e11
Thariq Ifthikar <thariqifthikar@gmail.com>
2022-05-02 21:19:08 +0530

    created Cat component and sample view All Cats component

--- cattylove-client/src/App.js
+++ cattylove-client/src/App.js
@@ -3,11 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PageLayout from './containers/PageLayout/PageLayout';
 import AddCat from './containers/AddCat/AddCat';
 import Admin from './containers/Admin/Admin';
-<<<<<<< HEAD
 import Cat from './containers/Cat/Cat';
-=======
 import EditCat from './containers/EditCat/EditCat';
->>>>>>> 2151765ec724eb60c216864880b3fae023f534b5
 
 import './App.css'
 
