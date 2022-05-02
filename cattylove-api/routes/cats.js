13f2621431eea5c6de2f72eafbbe75589ce315ea
Thariq Ifthikar <thariqifthikar@gmail.com>
2022-05-03 02:04:56 +0530

    added view all cats from database, updated display cards

--- cattylove-api/routes/cats.js
+++ cattylove-api/routes/cats.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const Cat = require('../models/cat')
+const User = require('../models/user')
 const Comment = require('../models/comment')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-
+    
+    console.log("request received " + new Date().toISOString())
+    try{
+        let cats = await Cat.find()
+        let users = await User.find()
+        res.send(cats)
+    }
+    catch(ex){
+        res.status(500).send("Error: " + ex.message)
+    }
 })
 
 module.exports = router
